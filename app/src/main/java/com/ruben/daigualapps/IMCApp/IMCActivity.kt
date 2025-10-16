8597a8af7c52e608a6ecf799ec606bba8eb6af41
Profesor <ruben.jarne@cpilosenlaces.com>
2025-10-16 20:56:06 +0200

    Lógica views Male y Female IMC App terminada

--- app/src/main/java/com/ruben/daigualapps/IMCApp/IMCActivity.kt
+++ app/src/main/java/com/ruben/daigualapps/IMCApp/IMCActivity.kt
@@ -3,11 +3,19 @@ package com.ruben.daigualapps.IMCApp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ruben.daigualapps.R
 
 class IMCActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +24,37 @@ class IMCActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left+32, systemBars.top+32, systemBars.right+32, systemBars.bottom+32)
             insets
+        }
+
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById<CardView>(R.id.viewMale)
+        viewFemale = findViewById<CardView>(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener { setComponentColorMale() }
+        viewFemale.setOnClickListener { setComponentColorFemale() }
+    }
+
+    private fun setComponentColorMale() {
+        if(!isMaleSelected) {
+            viewMale.setCardBackgroundColor(getColor(R.color.background_component_selected))
+            viewFemale.setCardBackgroundColor(getColor(R.color.background_component))
+            isMaleSelected = true
+            isFemaleSelected = false
+        }
+    }
+
+    private fun setComponentColorFemale() {
+        if(!isFemaleSelected) {
+            viewFemale.setCardBackgroundColor(getColor(R.color.background_component_selected))
+            viewMale.setCardBackgroundColor(getColor(R.color.background_component))
+            isFemaleSelected = true
+            isMaleSelected = false
         }
     }
 }
